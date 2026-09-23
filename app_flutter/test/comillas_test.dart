2dbe08cfaf58aa5c4bbc6ae7e3e1a679d8e1842a
Cesar Garcia <cesaremiliogarciagtz254@gmail.com>
2026-09-23 08:54:31 -0600

    chore(pubspec): rename package to conversor_epub and update metadata
    - Update package name to conversor_epub and set project description in pubspec.yaml
    - Update test import in comillas_test.dart to use package:conversor_epub

--- app_flutter/test/comillas_test.dart
+++ app_flutter/test/comillas_test.dart
@@ -1,6 +1,6 @@
 import 'dart:math';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:app_flutter/motor/limpieza.dart';
+import 'package:conversor_epub/motor/limpieza.dart';
 
 void main() {
   group('TestComillasCanonicas', () {
